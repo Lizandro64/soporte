f0041c439c3a2bacddcc4e89135bb85e88e67ec0
liznadro <lizandro.correa@outlook.com>
2024-05-13 14:23:00 -0400

    ok  server

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 
 app.set('views', join(__dirname, ' views'))
-app.set('vew engine', 'ejs');
+app.set('view engine', 'ejs');
+
+//res.render('archivo.ejs', { index });
 
 
 app.get ('/', (rep,res) => res.render('index'));
-
+app.get ('/home', (rep,res) => res.render('home'));
 
 app.listen(3000);
 console.log('Server is listening on port', 3000)
