de23e1e760b7f3fb5e4eefb20de26765f2b5bd9a
Lizandro <lizandro@hostv6.entelchile.net>
2024-05-12 22:49:44 -0400

    montado de sericio

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 
 
 import express from 'express'
-
+import {dirname} from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express ()
 
+dirname(fileURLToPath(import.meta.url))
+
+
+app.set('views', )
 app.set('vew engine', 'ejs');
 
 
-app.get ('/', (rep,res) => res.send('hello word'));
+app.get ('/', (rep,res) => res.render('index'));
 
 
 app.listen(3000);
