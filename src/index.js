1c2f9643939998e0d4a090cb79deb580cbe8bcb6
liznadro <lizandro.correa@outlook.com>
2024-05-13 14:20:42 -0400

    server ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
@@ -7,7 +5,7 @@ import { fileURLToPath } from 'url'
 const app = express ()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-console.log(join( __dirname, '/views'))
+
 
 
 app.set('views', join(__dirname, ' views'))
@@ -18,7 +16,6 @@ app.get ('/', (rep,res) => res.render('index'));
 
 
 app.listen(3000);
-
 console.log('Server is listening on port', 3000)
 
 
