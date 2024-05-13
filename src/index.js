4237c912f41ad966836a5c33426821955e9b8414
Lizandro <lizandro@hostv6.entelchile.net>
2024-05-12 22:33:49 -0400

    Servidor

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import express from 'express'
 
 const app = express ()
 
+app.get ('/', (rep,res) => res.send('hello word'));
+
+
 app.listen(3000);
 
 console.log('Server is listening on port', 3000)
