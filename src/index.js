c2d732d80c48f34c8c46d10e4f9b1c620c8f11ae
liznadro <lizandro.correa@outlook.com>
2024-05-13 20:00:15 -0400

    paginas login y usario primera revicion

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 
 app.get ('/', (rep,res) => res.render('index'));
 app.get ('/home', (rep,res) => res.render('home'));
+app.get ('/ingreso', (rep,res) => res.render('ingreso'));
+
 
 app.listen(3000);
 console.log('Server is listening on port', 3000)
